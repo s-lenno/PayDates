70a02b5c5939ad32aadacc7864a0de4aa1faa918
s-lenno <101260740+s-lenno@users.noreply.github.com>
2024-04-23 15:00:59 +0100

    Add files via upload

---
+++ PayWeeksAndDates/Programv1.1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+public class PayWeeksGenerator
+{
+    private DateTime startDate;
+    private DateTime endDate;
+    private int weekIncrement;
+
+    public PayWeeksGenerator(DateTime startDate, DateTime endDate, int weekIncrement)
+    {
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.weekIncrement = weekIncrement;
+    }
+
+    public void GeneratePayWeeks()
+    {
+        DateTime currentDate = startDate;
+        int weekNumber = 9;
+        int lineNumber = 1;
+
+        while (currentDate <= endDate)
+        {
+            Console.WriteLine("Week " + $"{weekNumber} = {startDate.ToString("dd MMMM yyyy")}" + " = Payslip " + lineNumber);
+            currentDate = currentDate.AddDays(7 * weekIncrement);
+            weekNumber = (weekNumber % 52) + 1; // Revert to 1 if exceeds 52
+            lineNumber++;
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Example usage:
+        DateTime startDate = new DateTime(2024, 2, 29); // Thursday, 29th February 2024
+        DateTime endDate = new DateTime(2025, 4, 30); // April 30, 2025
+        int weekIncrement = 4; // Increment by 4 weeks
+
+        PayWeeksGenerator generator = new PayWeeksGenerator(startDate, endDate, weekIncrement);
+        generator.GeneratePayWeeks();
+    }
+}
